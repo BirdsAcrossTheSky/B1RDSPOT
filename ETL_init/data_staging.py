93a7031b196b12b45ad2a44b15b7d2e58099ecf0
Moonmist <piotrekv50@gmail.com>
2024-05-23 19:21:10 +0200

    moving cfg data to json + add comments

--- ETL_init/data_staging.py
+++ ETL_init/data_staging.py
@@ -1,51 +1,22 @@
 import psycopg2
 import configparser
+import json
 
 
+# getting connection parameters from .ini config file
 config = configparser.ConfigParser()
 config.read('conf/db_config.ini')
 conn_paramas = config['postgresql']
 
-# List of dictionaries to hold CSV paths and SQL copy commands
-table_dict_list = [
-    {
-        'name': 'list',
-        'csv': 'EXTRACT/data/to_db/list_db.csv',
-        'copy_sql': """
-COPY STAGE.LIST (LATIN_NAME, POLISH_NAME, ENGLISH_NAME, CATEGORY, STATUS)
-FROM stdin WITH CSV HEADER DELIMITER as ','
-"""
-    },
-    {
-        'name': 'annex',
-        'csv': 'EXTRACT/data/to_db/annex_db.csv',
-        'copy_sql': """
-COPY STAGE.ANNEX (LATIN_NAME, POLISH_NAME, ENGLISH_NAME, CATEGORY, STATUS)
-FROM stdin WITH CSV HEADER DELIMITER as ','
-"""
-    },
-    {
-        'name': 'category',
-        'csv': 'EXTRACT/data/to_db/category_db.csv',
-        'copy_sql': """
-COPY STAGE.CATEGORY (CATEGORY, DESCRIPTION)
-FROM stdin WITH CSV HEADER DELIMITER as ','
-"""
-    },
-    {
-        'name': 'status',
-        'csv': 'EXTRACT/data/to_db/status_db.csv',
-        'copy_sql': """
-COPY STAGE.STATUS (STATUS, DESCRIPTION)
-FROM stdin WITH CSV HEADER DELIMITER as ','
-"""
-    }
-]
+# getting table info from .json config_file
+with open('conf/table_config.json') as tab_cfg_f:
+    table_dict_list = json.load(tab_cfg_f)
 
-
+# loading imported data to database
 with psycopg2.connect(**conn_paramas) as conn:
     for table_dict in table_dict_list:
         with conn.cursor() as cur:
+            # deleting all the data from table
             del_sql = f"DELETE FROM STAGE.{table_dict['name']}"
             with open(table_dict['csv'], 'r') as f:
                 next(f)  # skipping the header
